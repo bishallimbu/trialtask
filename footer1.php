6c6b05ed31dbcae52d05a67fe7e1390b39ab3910
bishallimbu <37394156+bishallimbu@users.noreply.github.com>
2018-08-30 14:36:42 +0545

    Add files via upload

---
+++ footer1.php
@@ -0,0 +1,114 @@
+<html>
+<head>
+<link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="footercss/css/font-awesome.min.css" rel="stylesheet">
+    <link href="footercss/css/prettyPhoto.css" rel="stylesheet">
+    <link href="footercss/css/price-range.css" rel="stylesheet">
+    <link href="footercss/css/animate.css" rel="stylesheet">
+	<link href="footercss/css/main.css" rel="stylesheet">
+	<link href="footercss/css/responsive.css" rel="stylesheet">
+    <link rel="footercss/shortcut icon" href="images/ico/favicon.ico">
+</head>
+<body>
+<footer id="footer"><!--Footer-->
+		<div class="footer-top">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-2">
+						<div class="companyinfo">
+							<h2><span>FASHION</span>PALETTE</h2>
+							<p>Lalitpur, Kathmandu <br> Contact no. +977-9816010616 <br> +977-9849769053</p>
+						</div>
+					</div>
+					<div class="col-sm-7">
+						
+					</div>
+					<div class="col-sm-3">
+						<div class="address">
+							<img src="images/home/map.png" alt="" />
+							<p>Kupandol - Lalitpur, Kathmandu, Nepal</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<div class="footer-widget">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-2">
+						<div class="single-widget">
+							<h2>Service</h2>
+							<ul class="nav nav-pills nav-stacked">
+								<li><a href="#">Online Help</a></li>
+								<li><a href="#">Contact Us</a></li>
+								<li><a href="#">Order Status</a></li>
+								<li><a href="#">Get Location</a></li>
+								<li><a href="#">FAQ’s</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-2">
+						<div class="single-widget">
+							<h2>Quock Shop</h2>
+							<ul class="nav nav-pills nav-stacked">
+								<li><a href="#">Bridal wear</a></li>
+								<li><a href="#">Salwar Kameez</a></li>
+								<li><a href="#">Kurtis & Tops</a></li>
+								<li><a href="#">Sherwani</a></li>
+								<li><a href="#">Embroidered Clothing</a></li>
+							</ul>
+						</div>
+					</div>
+					<!-- <div class="col-sm-2">
+						<div class="single-widget">
+							<h2>Policies</h2>
+							<ul class="nav nav-pills nav-stacked">
+								<li><a href="#">Terms of Use</a></li>
+								<li><a href="#">Privecy Policy</a></li>
+								<li><a href="#">Refund Policy</a></li>
+								<li><a href="#">Billing System</a></li>
+								<li><a href="#">Ticket System</a></li>
+							</ul>
+						</div>
+					</div> -->
+					<div class="col-sm-3">
+						<div class="single-widget">
+							<h2>About Fashion Palette</h2>
+							<ul class="nav nav-pills nav-stacked">
+								<li><a href="#">Fashion Palette Information</a></li>
+								<li><a href="#">Careers</a></li>
+								<li><a href="#">Store Location</a></li>
+								<li><a href="#">About Events</a></li>
+								<li><a href="#">Copyright</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-4 col-sm-offset-1">
+						<div class="single-widget">
+							<h2>About Fashion Palette Updates</h2>
+							<form action="#" class="searchform">
+								<input type="text" placeholder="Your email address" />
+								<button type="submit" class="btn btn-default"><i class="fa fa-arrow-circle-o-right"></i></button>
+								<p>Get the most recent updates from <br />our site and be updated your self...</p>
+							</form>
+						</div>
+					</div>
+					
+				</div>
+			</div>
+		</div>
+		
+		<div class="footer-bottom">
+			<div class="container">
+				<div class="row">
+					<p class="footer1" style=" text-align: center;">Copyright © 2013 FASHION PALETTE Pvt. All rights reserved.</p>
+					
+				</div>
+			</div>
+		</div>
+		
+	</footer><!--/Footer-->
+
+</body>
+</html>
